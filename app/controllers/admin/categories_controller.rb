5071e4b42837e53519afe30d02565417f938f278
Hane <w60241@gmail.com>
2017-12-04 22:14:25 +0900

    刪除所有版本紀錄

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,50 @@
+class Admin::CategoriesController < Admin::BaseController
+  before_action :set_category, only: [:update, :destroy]
+
+  def index
+    @categories = Category.all
+
+    if params[:id]
+      set_category
+    else
+      @category = Category.new
+    end
+  end
+
+  def create
+   @category = Category.new(category_params)
+   if @category.save
+     flash[:notice] = "category was successfully created"
+     redirect_to admin_categories_path
+   else
+     @categories = Category.all
+     render :index
+   end
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "category was successfully updated"
+      redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:alert] = "category was successfully deleted"
+    redirect_to admin_categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,46 @@
+class Admin::RestaurantsController < Admin::BaseController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.page(params[:page]).per(10)
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "restaurant was successfully updated"
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    flash[:alert] = "restaurant was deleted"
+    redirect_to admin_restaurants_path
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image, :category_id)
+  end
+end
